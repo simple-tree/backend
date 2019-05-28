e28f5b19a8af7ca8d896eb32f861f1334e57bede
thanhthanchuc <thanhthanchuc@gmail.com>
2019-05-28 22:33:35 +0700

    fix: cors

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const trees = require('./routers/tree');
 const cors = require('cors');
 
 const app = express();
-
 app.use(express.json());
+app.use(cors())
+
+app.all('*',(req, res, next) => {
+  const origin = req.get('origin'); 
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use('/', trees);
 
